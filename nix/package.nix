0898e05ec10c7f0e7caba9254691e77144b0b168
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-05-30 17:15:52 +0100

    i fucked up

--- nix/package.nix
+++ nix/package.nix
@@ -2,7 +2,14 @@
 
 pkgs.rustPlatform.buildRustPackage {
     pname = "dev";
-    version = "0.1";
-    cargoLock.lockFile = ./Cargo.lock;
-    src = pkgs.lib.cleanSource ./.;
+    version = "0.2";
+    cargoLock.lockFile = ../Cargo.lock;
+    src = pkgs.lib.cleanSource ../.;
+
+
+    #install bash completion
+    postInstall = ''
+        #install -Dm644 target/release/build/my-rust-app/out/my-rust-app.bash \
+        #    $out/share/bash-completion/completions/my-rust-app
+    '';
 }
