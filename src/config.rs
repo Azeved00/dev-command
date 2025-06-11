05258e859f8c9df7d0e5cc9c7299abf5c2b8e5ac
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-06-11 21:38:13 +0100

    improved code readibility of config definition, added git option (that does not do anything yet)

--- src/config.rs
+++ src/config.rs
@@ -6,14 +6,45 @@ use std::{
     path::PathBuf,
 };
 
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Window{
+    #[serde(default = "default_title")]
+    pub title: String,
+    #[serde(default = "default_nix_shell_name")]
+    pub nix_shell: String,
+    #[serde(default = "default_nix_rename")]
+    pub nix_rename: bool,
+}
+
+
 #[derive(Deserialize, Clone, Debug)]
 pub struct Session{
     #[serde(default = "default_windows")]
     pub windows: Vec<Window>,
     pub path: String,
+
     #[serde(default = "default_title")]
     pub title: String,
+
+    #[serde(default = "default_git")]
+    pub git: bool,
 }
+
+#[derive(Deserialize)]
+pub struct Config{
+    pub sessions: HashMap<String, Session>,
+}
+
+impl Config {
+    pub fn get_config(path:PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
+        let content = fs::read_to_string(path)?; 
+        let config: Config = toml::from_str(&content)?;
+        Ok(config)
+    }
+}
+//-------------------------------- DEFAULTS -----------------------------------
+
 pub fn default_windows() -> Vec<Window> { 
     vec![
         Window {
@@ -26,26 +57,4 @@ pub fn default_windows() -> Vec<Window> {
 fn default_title() -> String { "".to_string() }
 fn default_nix_rename() -> bool { false }
 fn default_nix_shell_name() -> String { "".to_string() }
-
-#[derive(Deserialize)]
-pub struct Config{
-    pub sessions: HashMap<String, Session>,
-}
-
-#[derive(Deserialize, Clone, Debug)]
-pub struct Window{
-    #[serde(default = "default_title")]
-    pub title: String,
-    #[serde(default = "default_nix_shell_name")]
-    pub nix_shell: String,
-    #[serde(default = "default_nix_rename")]
-    pub nix_rename: bool,
-}
-
-impl Config {
-    pub fn get_config(path:PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
-        let content = fs::read_to_string(path)?; 
-        let config: Config = toml::from_str(&content)?;
-        Ok(config)
-    }
-}
+fn default_git() -> bool {false}
