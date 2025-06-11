dba315f9c9822b69f48f27e5ac7e1776d624b82f
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-06-12 00:42:16 +0100

    bumped version

--- nix/package.nix
+++ nix/package.nix
@@ -2,7 +2,7 @@
 
 pkgs.rustPlatform.buildRustPackage {
     pname = "dev";
-    version = "0.2.1";
+    version = "1.0.0";
     cargoLock.lockFile = ../Cargo.lock;
     src = pkgs.lib.cleanSource ../.;
 
